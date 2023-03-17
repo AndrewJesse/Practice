f3c85de1558f329789c953e00d50c4779985a1a4
A.J. Marchese <aj.marchese@outlook.com>
2023-03-17 08:30:14 -0700

    lambda an linq

--- Lambdas/Program.cs
+++ Lambdas/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Linq;
+
 
 namespace Lambdas
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // Lambda expression to filter even numbers
+            var evenNumbers = numbers.Where(n => n % 2 == 0);
+
+            var newLinq = numbers.Sum(n => n);
+
+            Console.WriteLine("Even numbers:");
+            foreach (var number in evenNumbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.WriteLine(newLinq);
         }
+
     }
 }
