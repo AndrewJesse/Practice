ebf07368da13d0cb1591f141c2ccd80816dc396e
A.J. Marchese <aj.marchese@outlook.com>
2023-03-18 09:19:57 -0700

    made repository pattern

--- Lambdas/Program.cs
+++ Lambdas/Program.cs
@@ -7,22 +7,44 @@ namespace Lambdas
 {
     class Program
     {
+        // This is a data model
+        class Book
+        {
+            public string Author { get; set; }
+            public int Pages { get; set; }
+            public string Genre { get; set; }
+            public string Title { get; set; }
+            public decimal Price { get; set; }
+        }
+        // This class holds the list of books
+        class BookRepository
+        {
+            private List<Book> allBooks = new List<Book>
+            {
+                new Book { Author = "Neal Stephensen", Pages = 322, Genre = "Fiction", Title = "Snow Crash", Price = 9.99m },
+                new Book { Author = "Stephen King", Pages = 249, Genre = "Mystery", Title = "The Shining", Price = 12.99m },
+                new Book { Author = "Frank Herber", Pages = 405, Genre = "Sci-Fi", Title = "Dune", Price = 15.99m },
+            };
+
+            public List<Book> GetAllBooks() => allBooks;
+        }
         static void Main(string[] args)
         {
-            // Create data set
-            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            // Create an instance of the BookRepository class
+            BookRepository bookRepository = new BookRepository();
 
-            // From the array above, return the numbers greater than 6
-            var newLinq = numbers.Where(n => n > 6);
+            // Get the list of books from the BookRepository instance
+            List<Book> allBooks = bookRepository.GetAllBooks();
 
-            // Instead of a for each loop
-            newLinq.ToList().ForEach(n => Console.WriteLine(n));
+            // Assign the List of books to an IEnumerable<Book> variable
+            IEnumerable<Book> bookEnumerable = allBooks;
 
-            // What the above method accomplishes
-            foreach (var n in newLinq)
-            {
-                Console.WriteLine(n);
-            }
+            // Print the details of each book
+            allBooks.ForEach(n => Console.WriteLine($"Title: {n.Title}, Author: {n.Author}, Genre: {n.Genre}, Pages: {n.Pages}, Price: {n.Price:C}"));
+
         }
+
+
+
     }
 }
