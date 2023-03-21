1b9771cf16ce6209bd80fa624048a8dbde8e3789
A.J. Marchese <aj.marchese@outlook.com>
2023-03-21 08:48:11 -0700

    added As project

---
+++ As/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace As
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object[] array = new object[] { "hello", 123, true, 4.5 };
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                string str = array[i] as string;
+                if (str != null)
+                {
+                    Console.WriteLine("Element " + i + " is a string: " + str);
+                }
+                else
+                {
+                    Console.WriteLine("Element " + i + " is not a string.");
+                }
+            }
+        }
+    }
+}
