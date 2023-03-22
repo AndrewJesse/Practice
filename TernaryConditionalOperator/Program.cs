11bee09cab2b5ed16050352957d56ae99874ab02
A.J. Marchese <aj.marchese@outlook.com>
2023-03-22 08:56:23 -0700

    added ternary operator

---
+++ TernaryConditionalOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TernaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your age: ");
+            int age;
+            bool isValid = int.TryParse(Console.ReadLine(), out age);
+
+            if (!isValid)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                return;
+            }
+
+            string ageCategory = age >= 18 ? "adult" : "minor";
+            Console.WriteLine($"You are an {ageCategory}.");
+        }
+    }
+}
