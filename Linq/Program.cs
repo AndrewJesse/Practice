dbaf750aa3fd28d191a6c013a4cdc36aa6c195fb
A.J. Marchese <aj.marchese@outlook.com>
2023-03-23 08:35:54 -0700

    added Linq project

---
+++ Linq/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+            Console.WriteLine("Original list of numbers:");
+            Display(numbers);
+
+            Console.WriteLine("\nEven numbers:");
+            var evenNumbers = numbers.Where(n => n % 2 == 0);
+            Display(evenNumbers);
+
+            Console.WriteLine("\nNumbers greater than 50:");
+            var greaterThanFifty = numbers.Where(n => n > 50);
+            Display(greaterThanFifty);
+
+            Console.WriteLine("\nNumbers squared:");
+            var squaredNumbers = numbers.Select(n => n * n);
+            Display(squaredNumbers);
+
+            Console.ReadLine();
+        }
+
+        static void Display(IEnumerable<int> numbers)
+        {
+            foreach (var number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
