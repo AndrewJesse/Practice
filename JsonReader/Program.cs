d43fef7adccaeeddb00d0e5205929c991e453e4f
A.J. Marchese <aj.marchese@outlook.com>
2023-03-24 08:26:00 -0700

    added json project

---
+++ JsonReader/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Linq;
+
+namespace JsonReaderApp
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonFilePath = "ContactInformation.json";
+            string jsonContent = File.ReadAllText(jsonFilePath);
+
+            List<Person> people = JsonSerializer.Deserialize<List<Person>>(jsonContent);
+
+            Console.WriteLine("List of people:");
+            Console.WriteLine("---------------------");
+
+            foreach (var person in people)
+            {
+                Console.WriteLine($"Name: {person.Name}");
+                Console.WriteLine($"Age: {person.Age}");
+                Console.WriteLine($"Email: {person.Email}");
+                Console.WriteLine("---------------------");
+            }
+        }
+    }
+}
